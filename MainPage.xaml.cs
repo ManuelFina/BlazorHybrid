36327bcb3a400f1b25b76ca12a37a9d664aa5fd1
Manuel Fina <finamanuelmateo@gmail.com>
2025-06-22 15:02:17 -0300

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TrabajoPracticoTres
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
